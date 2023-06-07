a4eeb0b1409e2e082c85ba8074ddc68ffcbb546f
Tigist Taylor <taylortigist74@gmail.com>
2023-06-07 15:17:01 -0500

    final q4_TT

---
+++ q6_TT.sql
@@ -0,0 +1,12 @@
+ --Question 6:Find the player who had the most success stealing bases in 2016, where __success__ is measured as the percentage of stolen base attempts which are successful. (A stolen base attempt results either in a stolen base or being caught stealing.) Consider only players who attempted _at least_ 20 stolen bases.
+ SELECT p.namefirst, p.namelast, sb
+FROM batting AS b
+INNER JOIN people AS p
+    USING (playerid)
+WHERE b.yearid = 2016 
+    AND b.sb + b.cs >= 20
+GROUP BY p.namefirst, p.namelast, b.sb, p.playerid
+ORDER BY(sb::numeric * 100 / SUM(sb + cs)) DESC
+LIMIT 1;
+--Chris Owings
+
