fb50d1b27a99609c80f58b488fe48073b57b4680
April Mohamed <aprilmohamed@gmail.com>
2023-06-07 19:14:55 -0500

    q3 final

--- q3_april.sql
+++ q3_april.sql
@@ -1,5 +1,6 @@
----Q3 FN, LN, total salary for all players from Vanderbilt (salary DESC)
-SELECT namefirst, namelast, SUM(salary)
+---FN, LN, total salary for all players from Vanderbilt (salary DESC)
+---Highest earnings- David Price
+SELECT namefirst AS first_name, namelast AS last_name, SUM(salary)::numeric::money AS earnings
 FROM people INNER JOIN salaries USING(playerid)
 INNER JOIN collegeplaying USING(playerid)
 INNER JOIN schools USING(schoolid)
@@ -7,3 +8,11 @@ WHERE schoolname LIKE 'Vanderbilt%'
 GROUP BY namefirst, namelast
 ORDER BY SUM(salary) DESC;
 
+SELECT namefirst AS first_name, namelast AS last_name, SUM(salary)::numeric::money AS earnings
+FROM people INNER JOIN salaries USING(playerid)
+INNER JOIN collegeplaying USING(playerid)
+INNER JOIN schools USING(schoolid)
+WHERE schoolname LIKE 'Vanderbilt%'
+GROUP BY namefirst, namelast
+ORDER BY SUM(salary) DESC
+LIMIT 1;
