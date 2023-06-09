99a019be5c492feeb5c71d679dad296f2d00de1c
Courtney Webb <thecourtneywebb@gmail.com>
2023-06-09 09:25:40 -0500

    q9 final

--- q9_cw.sql
+++ q9_cw.sql
@@ -1,32 +1,24 @@
 --9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? Give their full name and the teams that they were managing when they won the award.
-SELECT *
-FROM awardsmanagers;
 
-SELECT *
-FROM awardsmanagers
-WHERE awardid ILIKE '%TSN%'
-AND lgid = 'NL';
 
-SELECT *
-FROM awardsmanagers
-WHERE awardid ILIKE '%TSN%'
-AND lgid = 'AL';
+SELECT CONCAT(people.namefirst,' ', people.namelast) AS tsn_mgr_of_year, teams.name AS team, awardsmanagers.yearid AS year_won
+FROM managers 
+JOIN people 
+USING (playerid)
+JOIN awardsmanagers ON managers.playerid = awardsmanagers.playerid AND managers.yearid = awardsmanagers.yearid
+JOIN teams ON managers.yearid = teams.yearid AND managers.teamid = teams.teamid
+WHERE awardsmanagers.playerid IN (SELECT playerid
+				  					FROM awardsmanagers
+				  					WHERE awardid ILIKE '%tsn%'
+				  					AND lgid = 'NL')
+									AND awardsmanagers.playerid IN 
+									(SELECT playerid
+				  					FROM awardsmanagers
+				  					WHERE awardid ILIKE '%tsn%'
+				  					AND lgid = 'AL')	
+GROUP BY tsn_mgr_of_year, team, year_won
+ORDER BY tsn_mgr_of_year, year_won;
 
---final query--
 
-WITH mgr_id AS ((SELECT playerid
-					FROM awardsmanagers
-					WHERE awardid ILIKE '%TSN%'
-					AND lgid = 'NL')
-					INTERSECT
-					(SELECT playerid
-					FROM awardsmanagers
-					WHERE awardid ILIKE '%TSN%'
-					AND lgid = 'AL'))
-SELECT CONCAT(namefirst,' ', namelast) AS tsn_al_nl_mgr_of_year
-FROM mgr_id
-JOIN people USING(playerid);
-					
-					
 
-			
+									
